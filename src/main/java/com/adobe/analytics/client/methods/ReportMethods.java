16f7b1a335bdff2b59bbec177e8e3780421066bd
Jason Doucette <jason@thrustlabs.com>
2015-11-29 13:32:01 -0500

    Added Report.GetQueue method support

--- src/main/java/com/adobe/analytics/client/methods/ReportMethods.java
+++ src/main/java/com/adobe/analytics/client/methods/ReportMethods.java
@@ -3,11 +3,15 @@ package com.adobe.analytics.client.methods;
 import static com.adobe.analytics.client.JsonUtil.o;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.List;
 
 import com.adobe.analytics.client.AnalyticsClient;
 import com.adobe.analytics.client.domain.ReportDescription;
 import com.adobe.analytics.client.domain.ReportResponse;
+import com.adobe.analytics.client.domain.ReportQueueItem;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 
 public class ReportMethods {
 
@@ -25,5 +29,10 @@ public class ReportMethods {
 
 	public ReportResponse get(int reportId) throws IOException {
 		return client.callMethod("Report.Get", o("reportID", reportId), ReportResponse.class);
+	}
+
+	public List<ReportQueueItem> getQueue() throws IOException {
+        final Type type = new TypeToken<List<ReportQueueItem>>() {}.getType();
+		return client.callMethod("Report.GetQueue", null, type);
 	}
 }
